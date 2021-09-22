1011e718b473d7a63a7c7aac6307ecccce7ccdce
Darío Fernández <dariofedes@protonmail.com>
2021-09-22 03:49:50 +0200

    first example

--- src/com/codurance/Account.java
+++ src/com/codurance/Account.java
@@ -1,17 +1,31 @@
 package com.codurance;
 
+import java.util.List;
+
 public class Account {
 
-    public void deposit(int amount) {
+    List<String> operationsDate;
+    List<Integer> operationsAmount;
 
+    public void deposit(int amount) {
+        String date = getDate();
+
+        operationsAmount.add(amount);
+        operationsDate.add(date);
     }
 
     public void withdraw(int amount) {
+        String date = getDate();
 
+        operationsAmount.add(amount * -1);
+        operationsDate.add(date);
     }
+
 
     public void printStatement(int amount) {
-
     }
 
+    private String getDate() {
+        return "fake date";
+    }
 }
