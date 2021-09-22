17ecb706684d5d048fb1bb3b5c739cfc7cd84c32
Darío Fernández <dariofedes@protonmail.com>
2021-09-22 03:02:25 +0200

    initialize project

---
+++ src/com/codurance/Account.java
@@ -0,0 +1,17 @@
+package com.codurance;
+
+public class Account {
+
+    public void deposit(int amount) {
+
+    }
+
+    public void withdraw(int amount) {
+
+    }
+
+    public void printStatement(int amount) {
+
+    }
+
+}
